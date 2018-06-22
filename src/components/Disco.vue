20472566c186464cbd2493a92a28bf0c409d30a5
Makoto Henmi <mako.mail.g@gmail.com>
2018-06-22 18:34:41 +0900

    作品ページを作成

---
+++ src/components/Disco.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="disco">
+    <h1>DISCOGRAPHY</h1>
+    <p>★★★★★★★工事中★★★★★★★</p>
+    <img src="../assets/job_kouji_ojigi.png" alt="工事作業員のイラスト「おじぎ・お詫び」">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'disco',
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Top from '@/components/Top';
+import Disco from '@/components/Disco';
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Top',
       component: Top,
+    },
+    {
+      path: '/disco',
+      name: 'Disco',
+      component: Disco,
     },
   ],
 });
