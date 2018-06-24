957a790b11a847d7e5f48d193367e3777d9bbcab
Makoto Henmi <mako.mail.g@gmail.com>
2018-06-24 17:49:46 +0900

    URLから#を削除して404ページを作成

---
+++ src/components/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="not-found">
+    <h1>404 NOT FOUND</h1>
+    <p>このページは存在しません。</p>
+    <img src="../assets/nomikai_salaryman.png" alt="飲み会のイラストです。ビール飲みましょう">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'not-found',
+};
+</script>
+
+<style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,12 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Top from '@/components/Top';
 import Disco from '@/components/Disco';
+import NotFound from '@/components/NotFound';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
@@ -16,6 +18,11 @@ export default new Router({
       path: '/disco',
       name: 'Disco',
       component: Disco,
+    },
+    {
+      path: '*',
+      name: 'NotFound',
+      component: NotFound,
     },
   ],
 });
