b67609d81dab7c59bdaa09ba7ca007f7d7cea89f
Makoto Henmi <mako.mail.g@gmail.com>
2018-07-05 20:51:14 +0900

    一言掲示板実装

--- src/components/Bbs.vue
+++ src/components/Bbs.vue
@@ -1,15 +1,27 @@
 <template>
   <div class="bbs">
     <h1>一言掲示板</h1>
+    <p>
+      足跡や、あいさつ、キリ番報告、等に使用していただいて構いません。
+      <br>
+      誹謗中傷、個人情報などの書き込みはご遠慮ください。
+    </p>
     <div class="message-form">
-      <label for="messageInput">メッセージ</label>
-      <input type="text" id="messageInput" maxlength="20px" v-model="message">
+      <div class="message-form-group">
+        <label for="nameInput">HN(ハンドルネーム)</label>
+        <input type="text" id="nameInput" maxlength="20" v-model="name">
+      </div>
+      <div class="message-form-group">
+        <label for="messageInput">メッセージ</label>
+        <input type="text" id="messageInput" maxlength="100" v-model="message">
+      </div>
       <button type="button" @click="sendMessage">送信</button>
     </div>
     <div>
       <ul class="message-list">
         <li v-for="(item, key) in messageList" v-bind:key="key">
-          {{item.message}}
+          {{item.message}} by {{item.name}}
+          <span class="time-label">{{item.createdAt}}</span>
         </li>
       </ul>
     </div>
@@ -17,17 +29,21 @@
 </template>
 
 <script lang="ts">
+import moment from 'moment';
 import Vue from 'vue';
 import database from '../firebase-config.ts';
 
 
 interface Message {
+  name: string,
   message: string,
+  createdAt: Date,
   sortKey: number
 }
 
 interface BbsData {
   messageList: Message[],
+  name: string,
   message: string
 }
 
@@ -36,6 +52,7 @@ export default Vue.extend({
   data() :BbsData {
     return {
       messageList: [],
+      name: '',
       message: '',
     };
   },
@@ -59,13 +76,16 @@ export default Vue.extend({
       });
     },
     sendMessage() {
-      if (!this.message) return;
+      if (!this.name || !this.message) return;
 
       database.ref('messages/').push({
+        name: this.name,
         message: this.message,
+        createdAt: moment(new Date()).format('YYYY/MM/DD H:mm:ss'),
         sortKey: -(new Date()),
       });
 
+      this.name = '';
       this.message = '';
     },
   },
@@ -74,22 +94,35 @@ export default Vue.extend({
 
 <style scoped>
   .message-form {
-    display: flex;
-    justify-content: center;
+    padding: 10px;
+    box-sizing: border-box;
   }
 
-  .message-form label {
-    width: 5em;
-    font-weight: bold;
-    margin-right: 10px;
+  @media all and (min-width: 480px) {
+    .message-form {
+      width: 400px;
+      margin: auto;
+    }
   }
-  .message-form input {
+
+  .message-form-group {
+    margin-bottom: 10px;
+  }
+
+  .message-form-group label {
+    font-weight: bold;
+  }
+
+  .message-form-group input {
+    display: block;
+    box-sizing: border-box;
     padding: 5px;
+    width: 100%;
   }
 
   .message-form button {
     background-color: gray;
-    width: 50px;
+    width: 100px;
     color: white;
     padding: 5px;
     margin-right: 10px;
@@ -106,8 +139,15 @@ export default Vue.extend({
   }
 
   .message-list li {
+    position: relative;
     background-color: white;
     padding: 5px 10px;
     margin-bottom: 5px;
+  }
+
+  .message-list .time-label {
+    position: absolute;
+    top: 5px;
+    right: 10px;
   }
 </style>
