0e1f4bb41b1e8d1b6b1357607df19a97db4f2288
Tomohiko Himura <eiel.hal@gmail.com>
2018-07-24 22:30:37 +0900

    fix: typo

--- src/components/atoms/TbtLogo.vue
+++ src/components/atoms/TbtLogo.vue
@@ -7,7 +7,7 @@ import Vue from 'vue';
 import Component from 'vue-class-component';
 
 @Component
-export default class TbtLog extends Vue {}
+export default class TbtLogo extends Vue {}
 </script>
 
 <style module>
