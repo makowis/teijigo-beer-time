3e3b23d1a027b7882e7ec70ca5eb4c720818f4da
Makoto Henmi <mako.mail.g@gmail.com>
2018-10-23 19:18:53 +0900

    クロスフェード動画をTOPに追加

--- src/components/organisms/TbtCircleSpaceInfoSection.vue
+++ src/components/organisms/TbtCircleSpaceInfoSection.vue
@@ -1,7 +1,7 @@
 <template>
   <tbt-section>
     <tbt-h2>M3スペース配置</tbt-h2>
-    <table :class="$style.spaceTable">
+    <table border="1" :class="$style.spaceTable">
       <tr>
         <th>サークル名</th>
         <td>クレオフーガ定時後ビールタイム</td>
@@ -21,6 +21,10 @@
       <p>場所：東京流通センター(TRC)</p>
       <a href="http://www.m3net.jp/index.html">M3公式サイト</a>
     </tbt-section>
+    <tbt-section>
+      <tbt-h3>頒布CDクロスフェード</tbt-h3>
+      <tbt-hope-cross-fade :class="$style.crossFade" />
+    </tbt-section>
   </tbt-section>
 </template>
 
@@ -30,12 +34,14 @@ import Component from 'vue-class-component';
 import TbtH2 from '@/components/atoms/TbtH2.vue';
 import TbtH3 from '@/components/atoms/TbtH3.vue';
 import TbtSection from '@/components/atoms/TbtSection.vue';
+import TbtHopeCrossFade from '@/components/atoms/TbtHopeCrossFade.vue';
 
 @Component({
   components: {
     TbtH2,
     TbtH3,
     TbtSection,
+    TbtHopeCrossFade,
   },
 })
 export default class TbtCircleSpaceInfoSection extends Vue {}
@@ -45,13 +51,15 @@ export default class TbtCircleSpaceInfoSection extends Vue {}
 .spaceTable {
   max-width: 90%;
   margin: auto;
-  border: solid 1px black;
-  border-collapse: separate;
 }
 
 .spaceTable td,
 .spaceTable th {
-  border: solid 1px black;
   padding: 5px 10px;
+}
+
+.crossFade {
+  width: 90%;
+  margin: auto;
 }
 </style>
