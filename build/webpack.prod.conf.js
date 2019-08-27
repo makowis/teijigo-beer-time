e2d8c039f954de9e02b8b2fe50246c8afb6c9f1d
Makoto Henmi <mako.mail.g@gmail.com>
2019-08-27 14:07:02 +0900

    uglifyjsの設定変更

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -51,9 +51,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     }),
     new UglifyJsPlugin({
       uglifyOptions: {
-        compress: {
-          warnings: false
-        }
+        warnings: false
       },
       sourceMap: config.build.productionSourceMap,
       parallel: true
