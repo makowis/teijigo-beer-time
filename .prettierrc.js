21d5dd78930c0135f64b03fbf31ffcc781fb20c7
Tomohiko Himura <eiel.hal@gmail.com>
2019-10-12 14:13:05 +0900

    chore: 拡張子でパーサーわかるっぽい気がするので消してみる

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,14 +1,5 @@
 module.exports = {
   arrowParens: 'always',
-  parser: 'typescript',
   singleQuote: true,
   trailingComma: 'all',
-  overrides: [
-    {
-      files: '*.vue',
-      options: {
-        parser: 'vue'
-      }
-    }
-  ]
 };
