27a3f3b9c202c12c9fc3755bfa58905657d517f4
Makoto Henmi <mako.mail.g@gmail.com>
2019-10-25 09:29:48 +0900

    recycleの情報を追記

---
+++ src/components/atoms/TbtRecycleJacketImg.vue
@@ -0,0 +1,16 @@
+<template>
+  <img
+    src="@/assets/3st_mini_album_recycle_jacket.png"
+    alt="3st Mini Album「RECYCLE」のジャケット画像"
+  />
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component
+export default class TbtRecycleJacketImg extends Vue {}
+</script>
+
+<style module></style>
