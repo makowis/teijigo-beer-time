69fd7947bb0f1fa60d242f5d3e62e10e7dda216d
Makoto Henmi <mako.mail.g@gmail.com>
2020-11-19 19:54:39 +0900

    fix lint

--- src/components/organisms/TbtMenuSection.vue
+++ src/components/organisms/TbtMenuSection.vue
@@ -2,19 +2,13 @@
   <tbt-section>
     <tbt-h2>メニュー</tbt-h2>
     <p>
-      <tbt-disco-link>
-        ＞＞＞＞DISCOGRAPHY＜＜＜＜
-      </tbt-disco-link>
+      <tbt-disco-link> ＞＞＞＞DISCOGRAPHY＜＜＜＜ </tbt-disco-link>
     </p>
     <p>
-      <tbt-bbs-link>
-        ★★★★★一言掲示板★★★★★
-      </tbt-bbs-link>
+      <tbt-bbs-link> ★★★★★一言掲示板★★★★★ </tbt-bbs-link>
     </p>
     <p>
-      <tbt-member-link>
-        🍺🍺🍺🍺️メンバー紹介🍺🍺🍺🍺
-      </tbt-member-link>
+      <tbt-member-link> 🍺🍺🍺🍺️メンバー紹介🍺🍺🍺🍺 </tbt-member-link>
     </p>
   </tbt-section>
 </template>
