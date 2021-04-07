5bb96e8102cf05a93ffb8d8bce85d88ff7471230
makowis <mako.mail.g@gmail.com>
2021-04-07 13:07:13 +0900

    スペックを修正

--- test/unit/specs/pages/Index.spec.ts
+++ test/unit/specs/pages/Index.spec.ts
@@ -2,9 +2,15 @@ import { mount, RouterLinkStub } from '@vue/test-utils'
 import Index from '@/pages/index.vue';
 
 describe('index.vue', () => {
+  const mockMath = Object.create(global.Math);
+  mockMath.random = () => 1;
+
   const vm = mount(Index, {
     stubs: {
       NuxtLink: RouterLinkStub
+    },
+    mocks: {
+      Math: mockMath
     }
   }).vm;
 
