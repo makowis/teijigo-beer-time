f5908ebc84c8afc2170b92a76468a894d9801fb8
makom <mako.mail.g@gmail.com>
2023-06-23 11:37:17 +0900

    fix error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,22 @@
 export default defineNuxtConfig({
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: '定時後ビールタイム',
-    htmlAttrs: {
-      lang: 'ja',
-    },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content:
-          '定時後にビール飲みながら作曲するかというノリで集まったメンバーでやってる音楽サークルです。',
+  app: {
+    head: {
+      title: '定時後ビールタイム',
+      htmlAttrs: {
+        lang: 'ja',
       },
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            '定時後にビール飲みながら作曲するかというノリで集まったメンバーでやってる音楽サークルです。',
+        },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    }
   },
   devtools: { enabled: true }
 })
