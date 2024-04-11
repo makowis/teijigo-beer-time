337e7ed90e54e4b1360e38977f8378ef6e284e17
makowis <makowis@users.noreply.github.com>
2024-04-11 03:47:26 +0000

    deploy: 6d3e09e8b8ffca10a319ab539d5b8f10a8461f06

--- _nuxt/TbtH1.6497d9a8.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c as t,x as c,q as n}from"./entry.46a43d75.js";const l="_h1_1yiid_2",r={h1:l},a={};function _(s,f){return o(),t("h1",{class:n(s.$style.h1)},[c(s.$slots,"default")],2)}const d={$style:r},i=e(a,[["render",_],["__cssModules",d]]);export{i as T};
