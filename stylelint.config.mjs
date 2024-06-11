9831de1de05fdb3082850c4c4cb326240cf0a266
makom <mako.mail.g@gmail.com>
2024-06-11 11:16:55 +0900

    mjsに変更

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,4 +1,6 @@
-module.exports = {
+/** @type {import('stylelint').Config} */
+
+export default {
   extends: ['stylelint-config-standard', 'stylelint-config-recommended-vue'],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
