82a57a89758c0d037f161eafa77429ad13d1271c
makowis <makowis@users.noreply.github.com>
2024-10-30 05:24:03 +0000

    deploy: 7bdedaa4792f5f1d0f6c273da5f015156ea8e72d

--- _nuxt/B3lHNJk9.js
+++
@@ -1,9 +0,0 @@
-import{T as r}from"./B71wQNgc.js";import{n as s,o as t,c as n,b as c,w as l,d as i,a as e,F as p,s as m,u as h,t as d,p as _,e as f,_ as u}from"./DBE8rHBw.js";const g={name:"さかもとP",charge:"なばかりプロデューサー兼テイジゴビールタイマー",profile:"ビール大好き！！！！！！"},b={name:"ビール飲まない社員K",charge:`人の褌や自分の褌で相撲を取ったりする
-<br>
-（主にトラックメーカー）`,profile:`ビールじゃなくてハイボールなら飲みます。
-<br>
-定時後ハイボールタイム、よろしこ`},k={name:"yokizo",charge:"えらそーにする",profile:"リハビリ中で戦力になれないので、えらそーにしてます。"},T={name:"管理人MAKO",charge:"サイトの管理とお茶汲み",profile:`サイトの管理人です。
-<br>
-音楽作れないのでホムペ担当になりました。
-<br>
-昔懐かしい雰囲気のサイトを作って行きたいと思います！`},M={name:"WestTail",charge:"ProTools見習い",profile:"周囲の素晴らしい才能をリスペクトしつつ頑張りたいと思います"},y={name:"ケロ",charge:"ミュージさん",profile:"ウィットに富んだフレキシブルな音楽でナウなヤングにバカウケ"},B={name:"Gutch",charge:"曲に効果音を突っ込む",profile:"ビール飲めない！！！！！"},x={name:"みーやん",charge:"ハモるの大好きはもりびと",profile:"ボーカルじゃなくてコーラスが得意"},H={name:"台湾豆乳を飲む人",charge:"作業に参加しない係",profile:"ﾆﾎﾝｺﾞワカリマセン"},L={name:"マッキー",charge:"BPM200以上",profile:"お酒大好き！！！！！！"},w={name:"Ag+",charge:"テレアポ",profile:"どこでもアポとります！"},I=[g,b,k,M,y,B,x,H,L,w,T],S=a=>(_("data-v-68f966cf"),a=a(),f(),a),v={class:"member"},E={class:"member-table"},K=S(()=>e("thead",null,[e("tr",null,[e("th",null,"名前"),e("th",null,"担当"),e("th",null,"自己紹介")])],-1)),N=["innerHTML"],P=["innerHTML"],V=s({__name:"member",setup(a){return(z,A)=>(t(),n("div",v,[c(r,null,{default:l(()=>[i("MEMBER")]),_:1}),e("table",E,[K,e("tbody",null,[(t(!0),n(p,null,m(h(I),o=>(t(),n("tr",{key:o.name},[e("td",null,d(o.name),1),e("td",{innerHTML:o.charge},null,8,N),e("td",{innerHTML:o.profile},null,8,P)]))),128))])])]))}}),D=u(V,[["__scopeId","data-v-68f966cf"]]);export{D as default};
