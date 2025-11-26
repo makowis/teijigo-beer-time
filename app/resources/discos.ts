c81e6564b4878f36bcd6e97ac907cc289336e4c9
makowis <mako.mail.g@gmail.com>
2025-11-26 08:00:18 +0000

    refactor: HTML型を共通ファイルに切り出し
    - app/resources/types.ts を新規作成
    - HTML型とhtml()関数を types.ts に移動
    - discos.ts と members.ts は types.ts からインポート
    - html()を使っていなかった箇所を修正

--- app/resources/discos.ts
+++ app/resources/discos.ts
@@ -1,15 +1,5 @@
-/**
- * HTMLマークアップを含む文字列を表すブランドタイプ
- * 通常のstringとは区別され、HTMLとして意図された文字列のみを受け入れる
- */
-export type HTML = string & { readonly __brand: 'HTML' }
-
-/**
- * 文字列をHTML型に変換するヘルパー関数
- * @param value - HTML文字列
- * @returns HTML型の文字列
- */
-export const html = (value: string): HTML => value as HTML
+import type { HTML } from './types'
+import { html } from './types'
 
 export interface Disco {
   title: string // アルバム名
@@ -70,7 +60,7 @@ export const rising: Disco = {
       no: 1,
       name: 'RISING',
       artist: 'マッキー',
-      credit: '作詞・作曲・歌：マッキー',
+      credit: html('作詞・作曲・歌：マッキー'),
     },
     {
       no: 2,
@@ -107,7 +97,7 @@ export const battle: Disco = {
       no: 1,
       name: '作業中にソフトが落ちた',
       artist: 'マッキー',
-      credit: '作詞・作曲・歌：マッキー',
+      credit: html('作詞・作曲・歌：マッキー'),
     },
     {
       no: 2,
